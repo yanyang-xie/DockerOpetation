60f101cd67f1e7cde4ba5768d865aac03ac382e1
Yanyang <yanyang.xie@thistech.com>
2017-05-02 17:21:11 +0800

    upload docker operation

---
+++ src/docker_operation.py
@@ -0,0 +1,110 @@
+# -*- coding=utf-8 -*-
+# author: yanyang.xie@thistech.com
+import sys, os
+
+from utility import common_util
+from utility.docker_util import DockerOperation
+
+
+sys.path.append(os.path.join(os.path.split(os.path.realpath(__file__))[0], ".."))
+
+task_name, docker_server_url, docker_image, container_name, container_cpu, container_memory = ('','','','', 1024, '1g')
+container_ports, container_volumes = ({},{})
+operator = ''
+
+def read_config():
+    if len(sys.argv) < 4:
+        print 'Missing required parameters. task, conf folder and operation'
+        exit(1)
+    
+    global task_name, operator
+    task_name, config_folder, operator = sys.argv[1], sys.argv[2], sys.argv[3]
+    config_file = os.path.dirname(os.path.abspath(__file__)) + os.sep + config_folder + os.sep + 'config.properties'
+    print 'task:{}, config file:{}'.format(task_name, config_file)
+    
+    conf = common_util.load_properties(config_file)
+    task_conf = {}
+    for key, value in conf.items():
+        if key.find(task_name + '.') == 0:
+            task_conf[key.replace(task_name + '.', '')] = value
+    
+    return task_conf
+
+def validate_config(task_conf):
+    if operator not in ['start', 'stop', 'create', 'delete', 'update']:
+        print "unsupported operator [%s]. start/stop/create/delete/update" %(operator)
+        exit(1)
+    
+    global docker_server_url, docker_image, container_name, container_ports, container_volumes,container_cpu, container_memory
+    if not task_conf.has_key('docker.server.url') or common_util.get_config_value_by_key(task_conf, 'docker.server.url')=='':
+        print 'Docker server must be provided'
+        exit(1)
+    else:
+        docker_server_url = common_util.get_config_value_by_key(task_conf, 'docker.server.url')
+    
+    if not task_conf.has_key('docker.server.url'):
+        print 'Image and container name must be provided'
+        exit(1)
+    
+    if not task_conf.has_key('docker.image') or not task_conf.has_key('container.name'):
+        print 'Image and container name must be provided'
+        exit(1)
+    
+    docker_image = common_util.get_config_value_by_key(task_conf, 'docker.image')
+    container_name = common_util.get_config_value_by_key(task_conf, 'container.name')
+    container_cpu = int(common_util.get_config_value_by_key(task_conf, 'container.cpu', 1024))
+    container_memory = common_util.get_config_value_by_key(task_conf, 'container.memory', '1g')
+    
+    #{'8080/tcp': 8090,}
+    container_ports_string = common_util.get_config_value_by_key(task_conf, 'container.ports.map')
+    for container_ports_map in container_ports_string.split(','):
+        port_list = container_ports_map.split(':')
+        container_ports['%s/tcp' %(port_list[0])] = int(port_list[1])
+        
+    container_volumes_string = common_util.get_config_value_by_key(task_conf, 'container.volumes.map')
+    for container_volumes_map in container_volumes_string.split(','):
+        volumes_list = container_volumes_map.split(':')
+        container_volumes[volumes_list[0]] = {'bind': '%s' %(volumes_list[1]), 'mode': 'rw'}
+
+def create():
+    if docker_operation.exist_container(container_name):
+        raise Exception('A container named %s is existed, not create')
+    
+    container = docker_operation.create_container(docker_image, container_name=container_name, ports=container_ports, volumes=container_volumes, cpu_shares=container_cpu, mem_limit=container_memory)
+    #print container.id, container.name, container.status, container.attrs
+    return container
+
+def update():
+    delete()
+    create()
+
+def delete():
+    if docker_operation.exist_container(container_name):
+        container = docker_operation.get_container_by_name(container_name)
+        docker_operation.stop_container(container)
+        docker_operation.delete_container(container)
+    else:
+        print 'Not found container %s' %(container_name)
+
+def start():
+    if docker_operation.exist_container(container_name):
+        container = docker_operation.get_container_by_name(container_name)
+        docker_operation.start_container(container)
+    else:
+        raise Exception('Not found container %s' %(container_name))
+
+def stop():
+    if docker_operation.exist_container(container_name):
+        container = docker_operation.get_container_by_name(container_name)
+        docker_operation.stop_container(container)
+    else:
+        raise Exception('Not found container %s' %(container_name))
+
+if __name__ == '__main__':
+    task_conf = read_config()
+    validate_config(task_conf)
+    print 'Task conf for [%s]: %s | %s | %s | %s | %s | %s | %s' %(task_name, docker_image, container_name, container_cpu, container_memory, container_ports, container_volumes, docker_server_url)
+    
+    docker_operation = DockerOperation(docker_server_url)
+    eval('%s()' %operator)
+    print 'Finished to %s container' %(operator)
---
+++ src/utility/docker_util.py
@@ -0,0 +1,92 @@
+# -*- coding=utf-8 -*-
+# author: yanyang.xie@thistech.com
+
+#https://docker-py.readthedocs.io/en/stable
+#yum install docker
+
+import docker
+
+class DockerOperation():
+    
+    def __init__(self, base_url, version='1.24', timeout=3):
+        self.client = docker.DockerClient(base_url=base_url, version=version, timeout=timeout,)
+        
+    def get_image_list(self, name=None, all=False, filters=None):
+        return self.client.images.list(name, all, filters)
+    
+    def get_container_list(self, all=True, before=None, filters=None, limit=-1, since=None):
+        return self.client.containers.list(all, before, filters, limit, since)
+    
+    def get_container_list_by_status(self, status='running'):
+        if status not in ['restarting', 'running','paused', 'exited', ]:
+            return []
+        
+        return self.client.containers.list(all=False, filters={'status':status})
+    
+    def get_container_by_id(self, cid):
+        return self.client.containers.get(cid)
+    
+    def get_container_by_name(self, name):
+        return self.client.containers.get(name)
+    
+    def exist_container(self, name):
+        try:
+            return True if self.get_container_by_name(name) is not None else False
+        except:
+            return False
+    
+    def create_container(self, docker_image, detach=True, container_name=None, ports={}, volumes={}, cpu_shares=1024, mem_limit='1g', **kwargs):
+        # cpu_shares: CPU shares (relative weight). 1024 is 1 core CPU
+        # mem_limit(str or int) : Memory limit. Accepts float values or a string with a units identification char (100000b, 1000k, 128m, 1g). If a string is specified without a units character, bytes are assumed as an intended unit.
+        return self.client.containers.run(docker_image, detach=detach, name=container_name, ports=ports, volumes=volumes, cpu_shares=cpu_shares, mem_limit=mem_limit, **kwargs)
+    
+    def delete_container(self, container):
+        container.remove()
+    
+    def start_container(self, container):
+        container.start()
+    
+    def restart_container(self, container):
+        container.restart()
+    
+    def stop_container(self, container):
+        container.stop()
+    
+    def pause_container(self, container):
+        container.pause()
+    
+    def run_cmd_in_container(self, container, cmd):
+        container.exec_run(cmd)
+        
+    def update_container(self, container, **kwargs):
+        container.update(kwargs)
+
+
+if __name__ == '__main__':
+    docker_operation = DockerOperation('http://52.221.182.216:6732')
+    
+    docker_image='tomcat:8.0'
+    container_name = 'tomcat-123456'
+    container_ports={'8080/tcp': 8090} #8080 is container ip, 8090 is node ip which can be access by user
+    container_volumes={'/root/test/logs': {'bind': '/root/tomcat/logs', 'mode': 'rw'},}
+    
+    if docker_operation.exist_container(container_name):
+        container = docker_operation.get_container_by_name(container_name)
+        print container.name
+        
+        docker_operation.stop_container(container)
+        docker_operation.delete_container(container)
+    
+    try:
+        container = docker_operation.create_container(docker_image, container_name=container_name, ports=container_ports, volumes=container_volumes, cpu_shares=512, mem_limit='2g')
+        print container.id, container.name, container.status, container.attrs
+    except:
+        import traceback
+        traceback.print_exc()
+    
+    docker_operation.start_container(container)
+    print container.id, container.name, container.status, container.attrs
+    
+    container = docker_operation.get_container_by_name(container_name)
+    docker_operation.stop_container(container)
+    docker_operation.delete_container(container)
